bcb1976749d89fa0ed31b6f675b5c4f580f276e0
aklimchu <aklimchu@student.hive.fi>
2025-02-18 14:12:32 +0200

    edited the converter

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -4,7 +4,6 @@
 
 
 void ScalarConverter::convert(const std::string str) {
-    // Create an array of member function pointers
 	bool (*checkers[])(const std::string&) = {&ScalarConverter::isSpecialFloat, &ScalarConverter::isInteger, \
 		&ScalarConverter::isChar, &ScalarConverter::isFloat, &ScalarConverter::isDouble};
 
@@ -24,15 +23,17 @@ void ScalarConverter::convert(const std::string str) {
 
 	if (type_captured == false)
 	{
+		std::cout << "No type recognized" << std::endl; // delete
 		std::cout << "char: impossible" << std::endl;
 		std::cout << "int: impossible" << std::endl;
 		std::cout << "float: impossible" << std::endl;
 		std::cout << "double: impossible" << std::endl;
+		return;
 	}
 
 	switch (i)  {
 		case 0:
-			std::cout << "It's special float" << std::endl;
+			std::cout << "It's special float" << std::endl; // delete
 			std::cout << "char: impossible" << std::endl;
 			std::cout << "int: impossible" << std::endl;
 			res_float = std::stof(str);
@@ -45,35 +46,49 @@ void ScalarConverter::convert(const std::string str) {
 				res_int = std::stoi(str);
 			}
 			catch (const std::exception& e) {
-				std::cout << "int: " << "impossible" << std::endl;
+				std::cout << "char: impossible" << std::endl;
+				std::cout << "int: impossible" << std::endl;
+				std::cout << "float: impossible" << std::endl;
+				std::cout << "double: impossible" << std::endl;
+				break;
 			}
 			if (res_int < std::numeric_limits<char>::min() || res_int > std::numeric_limits<char>::max())
 				std::cout << "char: impossible" << std::endl;
+			else if (!std::isprint(static_cast<char>(res_int)))
+				std::cout << "char: " << "Non displayable" << std::endl;
 			else {
-				std::cout << "char: " << static_cast<char>(res_int) << std::endl;
+				std::cout << "char: '" << static_cast<char>(res_int) << "'" << std::endl;
 			}
 			std::cout << "int: " << res_int << std::endl;
 			std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(res_int) << "f" << std::endl;
 			std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(res_int) << std::endl << std::endl;
 			break;
 		case 2:
-			std::cout << "It's char" << std::endl;
+			std::cout << "It's char" << std::endl; // delete
 			res_char = str[0];
-			res_int = static_cast<int>(res_char);
-			res_float = static_cast<float>(res_char);
-			res_double = static_cast<double>(res_char);
+			std::cout << "char: '" << res_char << "'" << std::endl;
+			std::cout << "int: " << static_cast<int>(res_char) << std::endl;
+			std::cout << "float: " << static_cast<float>(res_char) << std::endl;
+			std::cout << "double: " << static_cast<double>(res_char) << std::endl;
 			break;
 		case 3:
+			std::cout << "It's float" << std::endl; // delete
 			try {
 				res_float = std::stof(str);
 			}
 			catch (const std::exception& e) {
-				std::cout << "float: " << "impossible" << std::endl;
+				std::cout << "char: impossible" << std::endl;
+				std::cout << "int: impossible" << std::endl;
+				std::cout << "float: impossible" << std::endl;
+				std::cout << "double: impossible" << std::endl;
+				break;
 			}
 			if (res_float < std::numeric_limits<char>::min() || res_float > std::numeric_limits<char>::max())
 				std::cout << "char: impossible" << std::endl;
+			else if (!std::isprint(static_cast<char>(res_float)))
+				std::cout << "char: " << "Non displayable" << std::endl;
 			else {
-				std::cout << "char: " << static_cast<char>(res_float) << std::endl;
+				std::cout << "char: '" << static_cast<char>(res_float) << "'" << std::endl;
 			}
 			res_double = static_cast<double>(res_float);
 			if (res_double < std::numeric_limits<int>::min() || res_double > std::numeric_limits<int>::max())
@@ -82,29 +97,30 @@ void ScalarConverter::convert(const std::string str) {
 				std::cout << "int: " << static_cast<int>(res_float) << std::endl;
 			}
 			std::cout << "float: " << std::fixed << std::setprecision(1) << res_float << "f" << std::endl;
-			if (res_float < std::numeric_limits<double>::min() || res_float > std::numeric_limits<double>::max())
+			if (res_float < std::numeric_limits<double>::lowest() || res_float > std::numeric_limits<double>::max())
 				std::cout << "double: impossible" << std::endl;
 			else {
 				std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(res_float) << std::endl << std::endl;				
 			}
 			break;
 		case 4:
-			std::cout << "It's double" << std::endl;
-			/* res_double = std::stod(str);
-			res_int = static_cast<int>(res_double);
-			res_char = static_cast<char>(res_double);
-			res_float = static_cast<float>(res_double);
-			break; */
+			std::cout << "It's double" << std::endl; // delete
 			try {
 				res_double = std::stod(str);
 			}
 			catch (const std::exception& e) {
-				std::cout << "double: " << "impossible" << std::endl;
+				std::cout << "char: impossible" << std::endl;
+				std::cout << "int: impossible" << std::endl;
+				std::cout << "float: impossible" << std::endl;
+				std::cout << "double: impossible" << std::endl;
+				break;
 			}
 			if (res_double < std::numeric_limits<char>::min() || res_double > std::numeric_limits<char>::max())
 				std::cout << "char: impossible" << std::endl;
+			else if (!std::isprint(static_cast<char>(res_double)))
+				std::cout << "char: " << "Non displayable" << std::endl;
 			else {
-				std::cout << "char: " << static_cast<char>(res_double) << std::endl;
+				std::cout << "char: '" << static_cast<char>(res_double) << "'" << std::endl;
 			}
 			if (res_double < std::numeric_limits<int>::min() || res_double > std::numeric_limits<int>::max())
 				std::cout << "int: impossible" << std::endl;
@@ -112,54 +128,18 @@ void ScalarConverter::convert(const std::string str) {
 				std::cout << "int: " << static_cast<int>(res_double) << std::endl;
 			}
 			std::cout << "double: " << std::fixed << std::setprecision(1) << res_double << std::endl << std::endl;				
-			if (res_double < std::numeric_limits<float>::min() || res_double > std::numeric_limits<float>::max())
+			if (res_double < std::numeric_limits<float>::lowest() || res_double > std::numeric_limits<float>::max())
 				std::cout << "float: impossible" << std::endl;
 			else {
 				std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(res_double) << "f" << std::endl << std::endl;				
 			}
 			break;
 	}
-
-	/* if (!std::isprint(res_char))
-		std::cout << "char: " << "Non displayable" << std::endl;
-	else {
-		try {
-			std::cout << "char: '" << res_char << "'" << std::endl;
-		}
-		catch (const std::exception & e) {
-			std::cout << "char: " << "impossible" << std::endl;
-		}
-	}
-	try {
-		std::cout << "int: " << res_int << std::endl;
-	}
-	catch (const std::exception & e) {
-		std::cout << "int: " << "impossible" << std::endl;
-	}
-	try {
-		std::cout << "float: " << std::fixed << std::setprecision(1) << res_float << "f" << std::endl;
-	}
-	catch (const std::exception & e) {
-		std::cout << "float: " << "impossible" << std::endl;
-	}
-	try {
-		std::cout << "double: " << std::fixed << std::setprecision(1) << res_double << std::endl << std::endl;
-	}
-	catch (const std::exception & e) {
-		std::cout << "double: " << "impossible" << std::endl << std::endl;
-	} */
 }
 
 bool ScalarConverter::isInteger(const std::string &str) {
     std::regex intRegex(R"(^[+-]?\d+$)");
-	if (std::regex_match(str, intRegex)) {
-		double check = std::stod(str);
-		if (check < std::numeric_limits<int>::min() || check > std::numeric_limits<int>::max())
-			return false;
-		else
-			return true;
-	}
-    return false;
+    return std::regex_match(str, intRegex);
 }
 
 bool ScalarConverter::isFloat(const std::string &str) {
@@ -168,7 +148,7 @@ bool ScalarConverter::isFloat(const std::string &str) {
 }
 
 bool ScalarConverter::isDouble(const std::string &str) {
-	std::regex doubleRegex(R"(^[+-]?(inf|nan)$|^[+-]?\d+(\.\d+([eE][+-]?\d+)?)?$)");
+	std::regex doubleRegex(R"(^[+-]?(inf|nan)$|^[+-]?\d*\.\d+([eE][+-]?\d+)?$)");
     return std::regex_match(str, doubleRegex);
 }
 
